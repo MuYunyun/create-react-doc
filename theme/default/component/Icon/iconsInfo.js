1c3844f572a242592e8e0ae185e89c1a19541051
MuYunyun <328375795@qq.com>
2020-06-14 13:25:35 +0800

    docs: add Icon for Menu

---
+++ theme/default/component/Icon/iconsInfo.js
@@ -0,0 +1,7 @@
+// both minified by https://github.com/svg/svgo, https://jakearchibald.github.io/svgomg/
+export const IconsInfo = {
+  folder:
+    '<svg viewBox="0 0 1024 1024"><defs/><path d="M838 151H450a94 94 0 00-94-91H184c-52 0-94 42-94 94v595c0 51 42 93 94 93h654c52 0 94-42 94-93V244c0-51-42-93-94-93zm-654-31h172c19 0 35 15 35 34v26c0 17 13 30 29 30h418c19 0 34 15 34 34v27H150V154c0-19 15-34 34-34zm654 663H184a34 34 0 01-34-34V330h722v419c0 19-15 34-34 34z"/></svg>',
+  file:
+    '<svg class="icon" viewBox="0 0 1024 1024"><defs/><path d="M512 320H256v-64h256v64zM256 512h448v-64H256v64zm0 128h448v-64H256v64zm0 128h448v-64H256v64zm640-480v608c0 35-29 64-64 64H192c-35 0-64-29-64-64V128c0-35 29-64 64-64h480l224 224zm-64 32L640 128H192v768h640V320z"/></svg>',
+};
