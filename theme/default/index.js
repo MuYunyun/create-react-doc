9dad57caf55accf232d4e7ae49d7cad2e84a6cb8
MuYunyun <mujue@pinduoduo.com>
2020-06-28 12:50:20 +0800

    feat: remove extra logic

--- theme/default/index.js
+++ theme/default/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import DocumentTitle from 'react-document-title';
 import BasicLayout from './layout/BasicLayout';
-// import IndexLayout from './layout/IndexLayout';
 import NoMatch from './component/NoMatch';
 import Loading from './component/Loading';
 import './index.less';
 
 // 获取首页路由参数
-// Markdown 配置 layout = 'IndexLayout'
 const getIndexProps = (menus = [], attr) => {
   menus.forEach((item) => {
     if (item && item.mdconf && item.mdconf.layout === 'IndexLayout') {
@@ -23,8 +21,6 @@ const getIndexProps = (menus = [], attr) => {
 
 // run in the Web/Router.js
 export default function (Lazyload, props) {
-  let indexRoute = null;
-
   const LoadableComponent = Lazyload({
     component: () => import('./routes/Pages'),
     LoadingComponent: Loading,
@@ -48,25 +44,6 @@ export default function (Lazyload, props) {
     if (indexProps) indexProps.mdconf.layout = 'IndexLayout';
   }
 
-  // const indexItem = {
-  //   path: '/',
-  //   mdconf: { ...indexProps.mdconf, layout: 'IndexLayout' },
-  //   props: { ...indexProps.props },
-  //   article: indexProps.article,
-  // };
-
-  // 首页路由放置路由数组中生成路由
-  // props.routeData.unshift({
-  //   ...indexItem,
-  //   component: LoadableComponent,
-  // });
-
-  // 获取首页路由
-  indexRoute = props.routeData.filter(item => item.mdconf && item.mdconf.layout === 'IndexLayout');
-
-  /**
-   * todo: index page is to use root's README default.
-   */
   return (
     <Switch>
       <Route
@@ -80,7 +57,6 @@ export default function (Lazyload, props) {
       <Route path="/"
         render={(routeProps) => {
           const { location: { pathname } } = routeProps;
-          // routeProps.indexProps = indexItem;
           let curentRoute = props.routeData.filter(item => item.path === pathname);
           let title = [];
           if (curentRoute.length > 0) {
@@ -88,21 +64,10 @@ export default function (Lazyload, props) {
             if (curentRoute.mdconf && curentRoute.mdconf.title && curentRoute.mdconf.layout !== 'IndexLayout') {
               title.push(curentRoute.mdconf.title);
             }
-            // if (indexItem.mdconf && indexItem.mdconf.title) {
-            //   title.push(indexItem.mdconf.title);
-            // }
             title = title.length > 1 ? title.join(' - ') : title.join('');
           } else {
             title = '404';
           }
-          console.log(
-            'routeProps',
-            routeProps,
-            'props',
-            props,
-            'indexRoute',
-            indexRoute
-          );
           return (
             <DocumentTitle title={title}>
               <BasicLayout {...routeProps} {...props} />
