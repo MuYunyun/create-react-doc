d8a51b858c03de91bdebcda0c30f4206e2d58ca5
MuYunyun <328375795@qq.com>
2020-07-08 13:23:02 +0800

    refactor: directory

--- create-react-doc-lerna/packages/crd-scripts/bin/react-doc.js
+++ create-react-doc-lerna/packages/crd-scripts/bin/react-doc.js
@@ -2,14 +2,14 @@
 const fs = require('fs-extra');
 const path = require('path');
 const program = require('commander');
-const initProject = require('../../../../src/commands/initProject');
-const initCache = require('../../../../src/utils/initCache');
-const Servers = require('../../../../src/server');
-const Build = require('../../../../src/build');
-const Deploy = require('../../../../src/deploy');
-const paths = require('../../../../src/conf/path');
-const pkg = require('../../../../package.json');
-const { getDocsConfig } = require('../../../../src/utils');
+const initProject = require('../src/commands/initProject');
+const initCache = require('../src/utils/initCache');
+const Servers = require('../src/server');
+const Build = require('../src/build');
+const Deploy = require('../src/deploy');
+const paths = require('../src/conf/path');
+const pkg = require('../package.json');
+const { getDocsConfig } = require('../src/utils');
 
 program
   .version(pkg.version, '-v, --version')
@@ -24,7 +24,7 @@ program
   .on('--help', () => {
     console.log('\n  Examples:');
     console.log();
-    console.log('    $ react-doc init xxx');
+    console.log('    $ react-doc xxx');
     console.log('    $ react-doc start');
     console.log('    $ react-doc build');
     console.log('    $ react-doc deploy');
