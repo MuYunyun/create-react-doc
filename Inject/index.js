3dd2cc4c2ed70bb61df01adbd2dcac069ef884d2
MuYunyun <328375795@qq.com>
2021-01-07 13:44:18 +0800

    feat: support math jax

--- Inject/index.js
+++ Inject/index.js
@@ -1,19 +1,29 @@
+/* eslint-disable no-empty */
 const inject = () => {
   // demo for using mathjax. see https://github.com/MuYunyun/create-react-doc/issues/63
-  window.MathJax = {
-    tex: {
-      inlineMath: [['$', '$'], ['\\(', '\\)']],
-    },
-    svg: {
-      fontCache: 'global',
-    },
-  };
-
   (function () {
+    window.MathJax = {
+      tex: {
+        inlineMath: [['$', '$'], ['\\(', '\\)']],
+      },
+      svg: {
+        fontCache: 'global',
+      },
+    };
     const script = document.createElement('script');
     script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js';
+    const tag = 'mathjax';
+    script.id = tag;
     script.async = true;
-    document.head.appendChild(script);
+    const scriptNode = document.getElementById(tag);
+    try {
+      if (!scriptNode) {
+        document.head.appendChild(script);
+      } else {
+        scriptNode.remove();
+        document.head.appendChild(script);
+      }
+    } catch (error) {}
   }());
 };
 
