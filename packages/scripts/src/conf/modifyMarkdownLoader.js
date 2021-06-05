926d47ed0f6eddb790fef95edaaf9c5828fbb0ad
MuYunyun <328375795@qq.com>
2021-06-05 21:44:01 +0800

    feat: support write cp in md

---
+++ packages/scripts/src/conf/modifyMarkdownLoader.js
@@ -0,0 +1,24 @@
+/**
+ * this webpack plugin is to modify markdown position.
+ */
+const loaderUtils = require('loader-utils')
+const PATH = require('path')
+const FS = require('fs')
+
+module.exports = function (source) {
+  const options = loaderUtils.getOptions(this) || {}
+  const { replace, sep, path, include, extensions } = options
+  let content = ''
+
+  let fpath = this.resourcePath.replace(sep || /___/g, PATH.sep)
+  if (path && replace && (!include || include.test(fpath))) {
+    fpath = fpath.replace(path, replace)
+    this.addDependency(fpath)
+  }
+
+  if (FS.existsSync(fpath)) {
+    content = FS.readFileSync(fpath)
+  }
+  // content = JSON.stringify(content).replace(/\u2028/g, '\\u2028').replace(/\u2029/g, '\\u2029');
+  return content
+}
