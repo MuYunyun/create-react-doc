f7e57bb9259e34b0142bf6b04f407bc0eb6669a6
MuYunyun <328375795@qq.com>
2021-06-09 13:40:29 +0800

    feat: create-react-doc 集成 MDX

--- packages/scripts/src/conf/modifyMarkdownLoader.js
+++ packages/scripts/src/conf/modifyMarkdownLoader.js
@@ -1,23 +1,23 @@
-/**
- * this webpack plugin is to modify markdown position.
- */
-const loaderUtils = require('loader-utils')
-const PATH = require('path')
-const FS = require('fs')
+// /**
+//  * this webpack plugin is to modify markdown position.
+//  */
+// const loaderUtils = require('loader-utils')
+// const PATH = require('path')
+// const FS = require('fs')
 
-module.exports = function (source) {
-  const options = loaderUtils.getOptions(this) || {}
-  const { replace, sep, path, include, extensions } = options
-  let content = ''
+// module.exports = function (source) {
+//   const options = loaderUtils.getOptions(this) || {}
+//   const { replace, sep, path, include, extensions } = options
+//   let content = ''
 
-  let fpath = this.resourcePath.replace(sep || /___/g, PATH.sep)
-  if (path && replace && (!include || include.test(fpath))) {
-    fpath = fpath.replace(path, replace)
-    this.addDependency(fpath)
-  }
+//   let fpath = this.resourcePath.replace(sep || /___/g, PATH.sep)
+//   if (path && replace && (!include || include.test(fpath))) {
+//     fpath = fpath.replace(path, replace)
+//     this.addDependency(fpath)
+//   }
 
-  if (FS.existsSync(fpath)) {
-    content = FS.readFileSync(fpath)
-  }
-  return content
-}
+//   if (FS.existsSync(fpath)) {
+//     content = FS.readFileSync(fpath)
+//   }
+//   return content
+// }
