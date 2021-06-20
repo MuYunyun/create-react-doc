55b27dbf1da92afb73c69bd18f9337fd7b8f3d25
MuYunyun <328375795@qq.com>
2021-06-20 18:34:15 +0800

    fix: build loss file

--- packages/scripts/src/conf/rawTreeReplaceLoader.js
+++ packages/scripts/src/conf/rawTreeReplaceLoader.js
@@ -4,21 +4,21 @@ const DirectoryTree = require('./node-directory-tree')
 const PATH = require('path')
 const { ifInGitIgnore } = require('../utils/index')
 
-function getAllWatchPath(arr, pathArr = []) {
-  arr.forEach((item) => {
-    const mdfilePathInProject = item.path.replace(
-      process.cwd() + PATH.sep,
-      '',
-    )
-    if (!ifInGitIgnore(mdfilePathInProject) && item.type === 'file') {
-      pathArr.push(item.path)
-    }
-    if (item.children && item.children.length > 0) {
-      pathArr.concat(getAllWatchPath(item.children, pathArr))
-    }
-  })
-  return pathArr
-}
+// function getAllWatchPath(arr, pathArr = []) {
+//   arr.forEach((item) => {
+//     const mdfilePathInProject = item.path.replace(
+//       process.cwd() + PATH.sep,
+//       '',
+//     )
+//     if (!ifInGitIgnore(mdfilePathInProject) && item.type === 'file') {
+//       pathArr.push(item.path)
+//     }
+//     if (item.children && item.children.length > 0) {
+//       pathArr.concat(getAllWatchPath(item.children, pathArr))
+//     }
+//   })
+//   return pathArr
+// }
 
 function replacePath(dirs, path) {
   for (let i = 0; i < dirs.length; i += 1) {
@@ -59,17 +59,18 @@ module.exports = function (source) {
   if (directoryTrees && (!include || include.test(this.resourcePath))) {
     const dirs = Array.isArray(dir) ? dir : [dir]
     const dirTree = dirs.map(path => DirectoryTree(path, otherProps))
-    content = dirTree
+    // content = dirTree
 
-    const filemd = getAllWatchPath(dirTree)
-    filemd.forEach((fileItem) => {
-      this.addDependency(fileItem)
-    })
+    // const filemd = getAllWatchPath(dirTree)
+    // filemd.forEach((fileItem) => {
+    //   this.addDependency(fileItem)
+    // })
     content = getRelativePath(
       dirTree,
       relativePath,
       dirs,
     )
+    console.log('content', content)
   }
   content = JSON.stringify(content)
     .replace(/\u2028/g, '\\u2028')
