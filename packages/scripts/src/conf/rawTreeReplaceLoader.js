8fc42529d54bcf89e55f0529ea72d318004033dc
MuYunyun <328375795@qq.com>
2021-06-22 13:35:10 +0800

    fix: build loss file

--- packages/scripts/src/conf/rawTreeReplaceLoader.js
+++ packages/scripts/src/conf/rawTreeReplaceLoader.js
@@ -1,24 +1,23 @@
-const loaderUtils = require('loader-utils')
-// fork https://github.com/react-doc/node-directory-tree-md/blob/master/lib/directory-tree-md.js
+// A variation of https://github.com/react-doc/node-directory-tree-md/blob/master/lib/directory-tree-md.js
 const DirectoryTree = require('./node-directory-tree')
 const PATH = require('path')
 const { ifInGitIgnore } = require('../utils/index')
 
-// function getAllWatchPath(arr, pathArr = []) {
-//   arr.forEach((item) => {
-//     const mdfilePathInProject = item.path.replace(
-//       process.cwd() + PATH.sep,
-//       '',
-//     )
-//     if (!ifInGitIgnore(mdfilePathInProject) && item.type === 'file') {
-//       pathArr.push(item.path)
-//     }
-//     if (item.children && item.children.length > 0) {
-//       pathArr.concat(getAllWatchPath(item.children, pathArr))
-//     }
-//   })
-//   return pathArr
-// }
+function getAllWatchPath(arr, pathArr = []) {
+  arr.forEach((item) => {
+    const mdfilePathInProject = item.path.replace(
+      process.cwd() + PATH.sep,
+      '',
+    )
+    if (!ifInGitIgnore(mdfilePathInProject) && item.type === 'file') {
+      pathArr.push(item.path)
+    }
+    if (item.children && item.children.length > 0) {
+      pathArr.concat(getAllWatchPath(item.children, pathArr))
+    }
+  })
+  return pathArr
+}
 
 function replacePath(dirs, path) {
   for (let i = 0; i < dirs.length; i += 1) {
@@ -51,7 +50,7 @@ function getRelativePath(arr, relativePath, dirs) {
 
 module.exports = function (source) {
   // get option config from webpack loader, here is https://github.com/MuYunyun/create-react-doc/blob/main/packages/scripts/src/conf/webpack.config.prod.js#L61-L70
-  const options = loaderUtils.getOptions(this) || {}
+  const options = this.getOptions() || {}
   const { include, directoryTrees } = options
   const { dir, relativePath, ...otherProps } = directoryTrees
   let content = typeof source === 'string' ? JSON.parse(source) : source
@@ -60,19 +59,23 @@ module.exports = function (source) {
   // Todo: https://webpack.js.org/api/loaders/#pitching-loader
   if (directoryTrees && (!include || include.test(this.resourcePath))) {
     const dirs = Array.isArray(dir) ? dir : [dir]
+    // todo: white some logic about route relative in getRelativePath
     const dirTree = dirs.map(path => DirectoryTree(path, otherProps))
+    // if (Array.isArray(dirTree)) {
+    //   console.log('dirTree', dirTree[3] && dirTree[3].children)
+    // }
     // content = dirTree
 
-    // const filemd = getAllWatchPath(dirTree)
-    // filemd.forEach((fileItem) => {
-    //   this.addDependency(fileItem)
-    // })
+    const filemd = getAllWatchPath(dirTree)
+    filemd.forEach((fileItem) => {
+      this.addDependency(fileItem)
+    })
     content = getRelativePath(
       dirTree,
       relativePath,
       dirs,
     )
-    console.log('content', content)
+    // console.log('content', content)
   }
   content = JSON.stringify(content)
     .replace(/\u2028/g, '\\u2028')
