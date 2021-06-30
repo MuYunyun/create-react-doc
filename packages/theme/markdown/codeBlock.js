934a53c01396b2d06fa0a951be01479cc2f0282e
MuYunyun <328375795@qq.com>
2021-06-30 18:57:44 +0800

    feat: try live code

--- packages/theme/markdown/codeBlock.js
+++ packages/theme/markdown/codeBlock.js
@@ -1,38 +1,34 @@
-/** @jsxRuntime classic /
-/* @jsx jsx */
 import Highlight, { defaultProps } from 'prism-react-renderer'
 import nightOwlLight from 'prism-react-renderer/themes/nightOwlLight'
 import { mdx } from '@mdx-js/react'
-// import {LiveProvider, LiveEditor, LiveError, LivePreview} from 'react-live'
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live'
 
 export default ({ children, className, live, render }) => {
   // const language = className.replace(/language-/, '')
 
-  // if (live) {
-  //   return (
-  //     <div style={{marginTop: '40px', backgroundColor: 'black'}}>
-  //       <LiveProvider
-  //         code={children.trim()}
-  //         transformCode={code => '/** @jsx mdx */' + code}
-  //         scope={{mdx}}
-  //       >
-  //         <LivePreview />
-  //         <LiveEditor />
-  //         <LiveError />
-  //       </LiveProvider>
-  //     </div>
-  //   )
-  // }
+  if (live) {
+    return (
+      <LiveProvider
+        code={children.trim()}
+        transformCode={code => `/** @jsx mdx */${code}`}
+        scope={{ mdx }}
+      >
+        <LivePreview />
+        <LiveEditor />
+        <LiveError />
+      </LiveProvider>
+    )
+  }
 
-  // if (render) {
-  //   return (
-  //     <div style={{ marginTop: '40px' }}>
-  //       <LiveProvider code={children}>
-  //         <LivePreview />
-  //       </LiveProvider>
-  //     </div>
-  //   )
-  // }
+  if (render) {
+    return (
+      <div style={{ marginTop: '40px' }}>
+        <LiveProvider code={children}>
+          <LivePreview />
+        </LiveProvider>
+      </div>
+    )
+  }
 
   return (
     <Highlight {...defaultProps} theme={nightOwlLight} code={children.trim()} language="javascript">
