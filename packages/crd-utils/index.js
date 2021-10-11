275c0663e695ab3835e143ee47c329223e97c2c6
MuYunyun <328375795@qq.com>
2021-10-11 13:43:14 +0800

    feat: support generate uuid

--- packages/crd-utils/index.js
+++ packages/crd-utils/index.js
@@ -34,11 +34,27 @@ const replaceFileContent = (path, source, target) => {
   })
 }
 
+const insertFrontMatter = () => {
+  // todo...
+}
+
+// generate a random string, length of it is n.
+const generateRandomId = (n) => {
+  const str = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  let res = ""
+  for (let i = 0; i < n; i++) {
+    const id = Math.ceil(Math.random() * 35)
+    res += str[id]
+  }
+  return res
+}
+
 module.exports = {
   resolveApp,
   resolveTool,
   getDocsConfig,
   replaceFileContent,
+  generateRandomId,
   // common paths
   docsGitIgnore: resolveApp('.gitignore'),
   docsBase: resolveApp(''),
