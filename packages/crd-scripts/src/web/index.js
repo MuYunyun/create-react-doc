daa2b95f447d69fb9b42098818728b036f0fe461
MuYunyun <328375795@qq.com>
2022-01-06 13:45:43 +0800

    feat: try new idea https://github.com/MuYunyun/create-react-doc/issues/103

--- packages/crd-scripts/src/web/index.js
+++ packages/crd-scripts/src/web/index.js
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom'
+// import ReactDOMServer from 'react-dom/server';
 import '@babel/polyfill'
 import { ifProd, ifPrerender } from 'crd-client-utils'
 import RouterRoot from './Router'
@@ -10,4 +11,6 @@ if (!ifProdRender) {
     <RouterRoot />,
     document.getElementById('root'),
   )
+} else {
+  // todo: render dynamic logic(such as menu) here.
 }
