fc668128332e24cf5ca53676ec103f45a01782b8
MuYunyun <328375795@qq.com>
2022-02-02 20:24:12 +0800

    chore: update env

--- packages/crd-client-utils/index.js
+++ packages/crd-client-utils/index.js
@@ -4,7 +4,9 @@ const ifDev = env === 'dev'
 const ifProd = env === 'prod'
 const ifPrerender = window.__PRERENDER_INJECTED && window.__PRERENDER_INJECTED.prerender
 
-const useEnhancedEffect = typeof window !== 'undefined'
+// Not only Prod env but also [some part of Dev env](https://github.com/MuYunyun/create-react-doc/blob/main/packages/crd-scripts/src/web/index.js#L10-L13)
+// need using useLayoutEffect. If meeting this case in the future, thinking about passing extra tag from <RouterRoot />.
+const useEnhancedEffect = ifProd
   ? useLayoutEffect
   : useEffect
 
