b97bf3c159fc194db455509c40cf4c353dfb8e52
MuYunyun <328375795@qq.com>
2022-04-02 23:33:01 +0800

    docs: update react version

--- packages/crd-scripts/src/web/index.js
+++ packages/crd-scripts/src/web/index.js
@@ -1,23 +1,24 @@
-import ReactDOM from 'react-dom'
-import ReactDOMServer from 'react-dom/server'
+import { renderToPipeableStream } from 'react-dom'
+import { hydrateRoot } from 'react-dom/client'
+import ReactDOMServer from 'react-dom/server';
 import '@babel/polyfill'
 import { ifDev, ifPrerender } from 'crd-client-utils'
 import RouterRoot from './Router'
 
 if (ifDev) {
   // dev render
-  document.getElementById('root').innerHTML = ReactDOMServer.renderToString(<RouterRoot />)
-  ReactDOM.hydrate(
-    <RouterRoot />,
+  document.getElementById('root').innerHTML = renderToPipeableStream(<RouterRoot />)
+  hydrateRoot(
     document.getElementById('root'),
+    <RouterRoot />,
   )
 } else if (ifPrerender) {
   // prerender
-  document.getElementById('root').innerHTML = ReactDOMServer.renderToString(<RouterRoot />)
+  document.getElementById('root').innerHTML = renderToPipeableStream(<RouterRoot />)
 } else {
   // prod render:
-  ReactDOM.hydrate(
-    <RouterRoot />,
+  hydrateRoot(
     document.getElementById('root'),
+    <RouterRoot />,
   )
 }
