8b3190eda31960a47e4ca3e5ce25d3c922920f2e
MuYunyun <328375795@qq.com>
2022-04-05 15:40:57 +0800

    chore: code review upgrade react-router-dom to v6

--- packages/crd-seed/component/NoMatch/index.js
+++ packages/crd-seed/component/NoMatch/index.js
@@ -1,25 +1,20 @@
-import { Outlet } from 'react-router-dom'
 import styles from './index.less'
 
 const NoMatch = () => {
-  console.log('test 404')
   // eslint-disable-next-line no-undef
   const { user, repo } = DOCSCONFIG || {}
   return (
-    <>
-      <table className={styles.noMatch}>
-        <tbody>
-          <tr>
-            <td>
-              <h1>404</h1>
-              <div>你似乎来到了没有知识存在的荒原...</div>
-              <section>在 github 访问<a href={`https://github.com/${user}/${repo}`}>该项目</a></section>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <Outlet />
-    </>
+    <table className={styles.noMatch}>
+      <tbody>
+        <tr>
+          <td>
+            <h1>404</h1>
+            <div>你似乎来到了没有知识存在的荒原...</div>
+            <section>在 github 访问<a href={`https://github.com/${user}/${repo}`}>该项目</a></section>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   )
 }
 
